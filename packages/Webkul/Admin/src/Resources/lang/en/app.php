8270153ff9e172e79b5b3f8b14655dc2ca5ab28a
nicolas moises <nicolasmoises4@gmail.com>
2026-05-04 13:21:35 -0300

    ITD-16: SE AGREGO TRADUCCION EN INLGES

--- packages/Webkul/Admin/src/Resources/lang/en/app.php
+++ packages/Webkul/Admin/src/Resources/lang/en/app.php
@@ -1979,6 +1979,16 @@ return [
                         'to' => 'To',
                     ],
                 ],
+
+                'stages' => [
+                    'won' => 'Won',
+                    'lost' => 'Lost',
+                    'need-more-info' => 'Need More Details',
+                    'closed-at' => 'Closed At',
+                    'won-value' => 'Won Value',
+                    'lost-reason' => 'Lost Reason',
+                    'save-btn' => 'Save',
+                ],
             ],
 
             'view-switcher' => [
