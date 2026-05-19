5b113d807324ae3121808ad4ea1cd02db4f2425b
nicolas moises <nicolasmoises4@gmail.com>
2026-05-19 11:55:22 -0300

    ITD-22: bug fixed} de crear person

--- packages/Webkul/Contact/src/Repositories/PersonRepository.php
+++ packages/Webkul/Contact/src/Repositories/PersonRepository.php
@@ -174,9 +174,11 @@ class PersonRepository extends Repository
         $data['unique_id'] = implode('|', $uniqueIdParts);
 
         if (isset($data['contact_numbers'])) {
-            $data['contact_numbers'] = collect($data['contact_numbers'])->filter(fn ($number) => ! is_null($number['value']))->toArray();
+            $data['contact_numbers'] = collect($data['contact_numbers'])->filter(fn($number) => ! is_null($number['value']))->toArray();
 
-            $data['unique_id'] .= '|'.$data['contact_numbers'][0]['value'];
+            if (! empty($data['contact_numbers'])) {
+                $data['unique_id'] .= '|' . reset($data['contact_numbers'])['value'];
+            }
         }
 
         return $data;
